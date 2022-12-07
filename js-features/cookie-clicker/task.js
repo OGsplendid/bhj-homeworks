2ca988b8a293ea2ad6e236b6f1dd7b58a96cd5ed
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2022-12-07 11:14:18 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,13 +1,13 @@
 let initialTime = new Date();
-
 let button = document.getElementById('cookie');
+
 button.onclick = () => {
   document.getElementById('clicker__counter').innerHTML++;
   button.width = 200;
   let currentTime = new Date();
-  let diffInSeconds = (currentTime - initialTime) * 60 * 24;
+  let diffInSeconds = (currentTime.getTime() - initialTime.getTime()) / 1000;
   let timing = diffInSeconds / clicker__counter.textContent;
-  document.getElementById('clicker__time').textContent = timing;
+  document.getElementById('clicker__time').textContent = timing.toFixed(2);
 };
 
 button.onmousedown = () => {
