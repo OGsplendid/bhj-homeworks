6e0bfcef665f23068eeac21872e15d59b8df6d44
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2022-12-09 10:35:44 +0300

    Update task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,9 +1,9 @@
-let currentCount = 59;
+let currentCount = 10;
 
 let countDown = setInterval(function() {
   document.getElementById("timer").innerHTML = currentCount;
   currentCount--;
-  if (currentCount <= 0) {
+  if (currentCount < 0) {
     alert('Вы победили в конкурсе!');
     clearInterval(countDown);
   }
@@ -13,19 +13,19 @@ let countDown = setInterval(function() {
 
 let currentHours, currentMinutes, currentSeconds;
 currentHours = 04;
-currentMinutes = 25;
-currentSeconds = 19;
+currentMinutes = 01;
+currentSeconds = 10;
 
 let theFinalCountdown = setInterval(function() {
   document.getElementById("timerHours").innerHTML = currentHours;
   document.getElementById("timerMinutes").innerHTML = currentMinutes;
   document.getElementById("timerSeconds").innerHTML = currentSeconds;
   currentSeconds--;
-  if (currentSeconds === 00) {
+  if (currentSeconds < 0) {
     currentMinutes--;
     currentSeconds = 59;
   }
-  if (currentMinutes === 00) {
+  if (currentMinutes < 0) {
     currentHours--;
     currentMinutes = 59;
   }
@@ -39,7 +39,7 @@ let theFinalCountdown = setInterval(function() {
 // let countDown = setInterval(function() {
 //   document.getElementById("timer").innerHTML = currentCount;
 //   currentCount--;
-//   if (currentCount <= 0) {
+//   if (currentCount < 0) {
 //     window.location.assign("http://www.mozilla.org");
 //     clearInterval(countDown);
 //   }
