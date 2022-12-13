bedc84f866ede99d84b511d9be22e1f79ff99bcd
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2022-12-13 22:00:28 +0300

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,16 @@
 let menuLinks = document.getElementsByClassName('menu__link');
 let arrOfMenuLinks = Array.from(menuLinks);
 
+let subMenues = document.getElementsByClassName('menu_sub');
+let arrOfSubMenues = Array.from(subMenues);
+
 for (let i = 0; i < arrOfMenuLinks.length; i++) {
   arrOfMenuLinks[i].onclick = function() {
-    if (arrOfMenuLinks[i].closest('ul').className === 'menu menu_sub') {
-      arrOfMenuLinks[i].closest('ul').className = 'menu menu_sub menu_active';
-      return false;
+    arrOfSubMenues.map(el => el.className = 'menu menu_sub');
+    let neigbour = arrOfMenuLinks[i].closest('li').querySelector('ul');
+    if (neigbour.className === 'menu menu_sub') {
+        neigbour.className = 'menu menu_sub menu_active';
+        return false;
     };
   };
 };
