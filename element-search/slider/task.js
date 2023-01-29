cd43c1ca7d12e5c9f79be37e10d7bce0d5807a64
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-01-29 19:07:22 +0300

    Update task.js

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,41 +1,43 @@
 let arrows = document.getElementsByClassName('slider__arrow');
-let arrOfArrows = Array.from('arrows');
+let arrOfArrows = Array.from(arrows);
 let previous = arrOfArrows[0];
 let next = arrOfArrows[1];
 
 let sliders = document.getElementsByClassName('slider__item');
-let arrOfSliders = Array.from('sliders');
+let arrOfSliders = Array.from(sliders);
+
+let dots = document.getElementsByClassName('slider__dot');
+let arrOfDots = Array.from(dots);
+
 let number = 0;
+let oldNumber;
+
+function activateSlide(number) {
+    arrOfSliders[oldNumber].className = 'slider__item';
+    arrOfSliders[number].className = 'slider__item slider__item_active';
+}
 
 previous.onclick = function() {
-    arrOfSliders[number].className = 'slider__item';
+    oldNumber = number;
     number--;
-    arrOfSliders[number].className = 'slider__item slider__item_active';
-    if (number < 0) {
-        arrOfSliders[0].className = 'slider__item';
-        number = arrOfSliders.length;
-        arrOfSliders[number].className = 'slider__item slider__item_active';
+    if (number === -1) {
+        number = arrOfSliders.length - 1;
     }
+    activateSlide(number);
 }
 
 next.onclick = function() {
-    arrOfSliders[number].className = 'slider__item';
+    oldNumber = number;
     number++;
-    arrOfSliders[number].className = 'slider__item slider__item_active';
-    if (number > arrOfSliders.length) {
-        arrOfSliders[arrOfSliders.length].className = 'slider__item';
+    if (number > arrOfSliders.length - 1) {
         number = 0;
-        arrOfSliders[number].className = 'slider__item slider__item_active';
     }
+    activateSlide(number);
 }
-
-
-let dots = document.getElementsByClassName('slider__dot');
-let arrOfDots = Array.from('dots');
 
 for (let i = 0; i < arrOfDots.length; i++) {
     arrOfDots[i].onclick = function() {
-        arrOfDots.map(el => el.className = 'slider__dot');
-        arrOfDots[i].className = 'slider__dot slider__dot_active';
+        arrOfSliders.map(el => el.className = 'slider__item');
+        activateSlide(i);
     }
 }
