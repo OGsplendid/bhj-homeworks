24f53e86c0051aace639a3777370e0bcefb2d29f
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-01-29 17:56:54 +0300

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,12 +5,20 @@ let subMenues = document.getElementsByClassName('menu_sub');
 let arrOfSubMenues = Array.from(subMenues);
 
 for (let i = 0; i < arrOfMenuLinks.length; i++) {
+  let subMenuLink = arrOfMenuLinks[i].closest('.menu__item').querySelector('.menu_sub');
+
   arrOfMenuLinks[i].onclick = function() {
-    arrOfSubMenues.map(el => el.className = 'menu menu_sub');
-    let neigbour = arrOfMenuLinks[i].closest('li').querySelector('ul');
-    if (neigbour.className === 'menu menu_sub') {
-        neigbour.className = 'menu menu_sub menu_active';
-        return false;
+
+    if (arrOfSubMenues.some(el => el.className === 'menu menu_sub menu_active')) {
+      arrOfSubMenues.forEach(el => el.className = 'menu menu_sub');
     };
-  };
+
+    if (subMenuLink && subMenuLink.className === 'menu menu_sub') {
+      subMenuLink.className = 'menu menu_sub menu_active';
+      return false;
+    } else if (subMenuLink && subMenuLink.className === 'menu menu_sub menu_active') {
+      subMenuLink.className = 'menu menu_sub';
+      return false;
+    }
+  }
 };
