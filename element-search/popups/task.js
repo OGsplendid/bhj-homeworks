c07fdfbb7aa380cc7bae42863f602d9a0bc3d70f
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-01-29 15:23:31 +0300

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,22 +1,19 @@
 let modalMain = document.getElementById('modal_main');
 modalMain.className = 'modal modal_active';
-let closeBtns = document.getElementsByClassName('modal__close');
-let firstElTimes = closeBtns[0];
-let secondElTimes = closeBtns[2];
-
-firstElTimes.onclick = () => {
-  modalMain.className = 'modal';
-};
-
-secondElTimes.onclick = () => {
-  modalSuccess.className = 'modal';
-  modalMain.className = 'modal';
-};
 
 let modalSuccess = document.getElementById('modal_success');
-let successEl = document.getElementsByClassName('show-success')[0];
+
+let closeBtns = document.getElementsByClassName('modal__close_times');
+let arrOfCloseBtns = Array.from(closeBtns);
+
+let successEl = document.querySelector('.show-success');
+
+for (let i = 0; i < arrOfCloseBtns.length; i++) {
+  arrOfCloseBtns[i].onclick = () => {
+    arrOfCloseBtns[i].closest('.modal').className = 'modal';
+  }
+}
 
 successEl.onclick = () => {
   modalSuccess.className = 'modal modal_active';
-  modalMain.className = 'modal';
-};
+}
