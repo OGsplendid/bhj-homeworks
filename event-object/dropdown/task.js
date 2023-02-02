a2cb336f13052a7a6d7ce37aaf9c40d50909a809
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-02-02 20:38:50 +0300

    Update task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,8 +1,7 @@
 let dropdownList = document.querySelector('.dropdown__list');
 let dropdownValue = document.querySelector('.dropdown__value');
 
-let dropdownLinks = document.getElementsByClassName('dropdown__link');
-let arrOfLinks = Array.from(dropdownLinks);
+let dropdownLinks = Array.from(document.getElementsByClassName('dropdown__link'));
 
 dropdownValue.onclick = function() {
     if (dropdownList.className === 'dropdown__list') {
@@ -12,10 +11,10 @@ dropdownValue.onclick = function() {
     }
 };
 
-for (let i = 0; i < arrOfLinks.length; i++) {
-    arrOfLinks[i].onclick = function() {
+for (let i = 0; i < dropdownLinks.length; i++) {
+    dropdownLinks[i].onclick = function() {
         dropdownList.className = 'dropdown__list';
-        dropdownValue.textContent = arrOfLinks[i].textContent;
+        dropdownValue.textContent = dropdownLinks[i].textContent;
         return false;
     };
 };
