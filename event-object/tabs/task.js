f658b9c2e992ffb2aaac716d50f4273d7a061351
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-02-03 15:35:53 +0300

    Update task.js

---
+++ event-object/tabs/task.js
@@ -0,0 +1,21 @@
+let tabs = document.getElementsByClassName('tab');
+let arrOfTabs = Array.from(document.getElementsByClassName('tab'));
+let tabContents = document.getElementsByClassName('tab__content');
+let arrOfTabContents = Array.from(tabContents);
+
+// function activateTab() {
+    
+// }
+
+for (let i = 0; i < tabs.length; i++) {
+    arrOfTabs[i].onclick = function() {
+        if (arrOfTabs[i].className === 'tab') {
+            arrOfTabs.forEach(el => el.className = 'tab');
+            arrOfTabContents.forEach(el => el.className = 'tab__content');
+            arrOfTabs[i].className = 'tab tab_active';
+            arrOfTabContents[i].className = 'tab__content tab__content_active';
+        } else {
+            return;
+        }
+    }
+}
