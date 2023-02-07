be092e95828d7b6f83142f25925b2ab267621775
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-02-07 19:28:29 +0300

    Update task.js

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,19 +3,11 @@ let arrOfTabs = Array.from(document.getElementsByClassName('tab'));
 let tabContents = document.getElementsByClassName('tab__content');
 let arrOfTabContents = Array.from(tabContents);
 
-// почему не выходит так:
-// let arrOfTabs = Array.from(document.getElementsByClassName('tab'));
-// let arrOfTabContents = Array.from(document.getElementsByClassName('tab__content'));
-
 for (let i = 0; i < tabs.length; i++) {
     arrOfTabs[i].onclick = function() {
-        if (arrOfTabs[i].className === 'tab') {
-            arrOfTabs.forEach(el => el.className = 'tab');
-            arrOfTabContents.forEach(el => el.className = 'tab__content');
-            arrOfTabs[i].className = 'tab tab_active';
-            arrOfTabContents[i].className = 'tab__content tab__content_active';
-        } else {
-            return;
-        }
+        arrOfTabs.forEach(el => el.className = 'tab');
+        arrOfTabContents.forEach(el => el.className = 'tab__content');
+        arrOfTabs[i].classList.toggle('tab_active');
+        arrOfTabContents[i].classList.toggle('tab__content_active');
     }
 }
