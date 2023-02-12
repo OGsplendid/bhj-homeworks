e09dd3842070cf2d9a1dadbedbfb6c374b174542
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-02-12 20:40:07 +0300

    Update task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -3,18 +3,32 @@ let dropdownValue = document.querySelector('.dropdown__value');
 
 let dropdownLinks = Array.from(document.getElementsByClassName('dropdown__link'));
 
+// новая версия:
+
 dropdownValue.onclick = function() {
-    if (dropdownList.className === 'dropdown__list') {
-        dropdownList.className = 'dropdown__list dropdown__list_active';
-    } else {
-        dropdownList.className = 'dropdown__list';
-    }
+    dropdownList.classList.toggle('dropdown__list_active');
 };
 
-for (let i = 0; i < dropdownLinks.length; i++) {
-    dropdownLinks[i].onclick = function() {
-        dropdownList.className = 'dropdown__list';
-        dropdownValue.textContent = dropdownLinks[i].textContent;
+for (let link of dropdownLinks) {
+    link.onclick = function() {
+        dropdownList.classList.toggle('dropdown__list_active');
+        dropdownValue.textContent = link.textContent;
         return false;
     };
-};
+}
+
+// dropdownValue.onclick = function() {
+//     if (dropdownList.className === 'dropdown__list') {
+//         dropdownList.className = 'dropdown__list dropdown__list_active';
+//     } else {
+//         dropdownList.className = 'dropdown__list';
+//     }
+// };
+
+// for (let i = 0; i < dropdownLinks.length; i++) {
+//     dropdownLinks[i].onclick = function() {
+//         dropdownList.className = 'dropdown__list';
+//         dropdownValue.textContent = dropdownLinks[i].textContent;
+//         return false;
+//     };
+// };
