097e6353f4ecafb01867f507682cfe490713ff2b
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-02-14 20:19:39 +0300

    Update task.js

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -2,15 +2,28 @@
 (() => {
     let rotators = Array.from(document.getElementsByClassName('rotator__case'));
     let index = 0;
-    let speed = +rotators[index].dataset.speed;
+    let speed = rotators[index].dataset.speed;
     // если не выполнить строчку ниже, то цвет первого сообщения чёрный
     rotators[index].style.color = rotators[index].dataset.color;
 
-    // здесь смотрел в консоли - значение скорости меняется
-    // setInterval(() => console.log(speed), 1000);
+    setInterval(() => {
+        console.log(speed);
+    }, 1000);
 
-    // так тоже пробовал:
-    // function changeText() {
+    setTimeout(() => {
+        setInterval(() => {
+            rotators[index].classList.remove('rotator__case_active');
+            index++;
+            if (index === rotators.length) {
+                index = 0;
+            };
+            rotators[index].classList.add('rotator__case_active');
+            rotators[index].style.color = rotators[index].dataset.color;
+            speed = rotators[index].dataset.speed;
+        }, 1000);
+    }, speed);
+
+    // setInterval(() => {
     //     rotators[index].classList.remove('rotator__case_active');
     //     index++;
     //     if (index === rotators.length) {
@@ -19,18 +32,6 @@
     //     rotators[index].classList.add('rotator__case_active');
     //     rotators[index].style.color = rotators[index].dataset.color;
     //     speed = +rotators[index].dataset.speed;
-    // }
-    // setInterval(changeText, speed);
-
-
-    setInterval(() => {
-        rotators[index].classList.remove('rotator__case_active');
-        index++;
-        if (index === rotators.length) {
-            index = 0;
-        };
-        rotators[index].classList.add('rotator__case_active');
-        rotators[index].style.color = rotators[index].dataset.color;
-        speed = +rotators[index].dataset.speed;
-    }, speed);
+    //     console.log(speed);
+    // }, 1000);
 })();
