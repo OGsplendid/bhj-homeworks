e590167d5149ea72d0831deef15febffa4e0afed
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-03-01 10:56:30 +0300

    Update task.js

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,9 +1,21 @@
 let products = Array.from(document.querySelectorAll('.product'));
 
 for (let product of products) {
+    let cartDiv = document.querySelector('.cart');
     let cart = document.querySelector('.cart__products');
     let quantity = product.querySelector('.product__quantity-value');
     let image = product.querySelector('.product__image');
+
+    cartDiv.onclick = function(event) {
+        let target = event.target;
+        console.log(target);
+        if (target.classList.contains('close__button')) {
+            target.closest('div').remove();
+        }
+        if (!cartDiv.querySelector('.cart__product')) {
+            cartDiv.style.display = 'none';
+        }
+    }
 
     function increaseValue() {
         quantity.textContent++;
@@ -22,34 +34,30 @@ for (let product of products) {
         let itemImage = image.cloneNode(false);
         itemImage.className = 'cart__product-image';
         item.insertAdjacentElement('afterBegin', itemImage);
-        item.insertAdjacentHTML('beforeEnd', '<div class="cart__product-count"></div>');
+        item.insertAdjacentHTML('beforeEnd', '<div class="cart__product-count"></div><a href="#" class="close__button">&times;</a>');
         item.querySelector('.cart__product-count').textContent = quantity.textContent.trim();
         return item;
     }
 
     function addProduct() {
+
         let item = createProduct();
+        let itemQuantity = Number(item.querySelector('.cart__product-count').textContent);
+        
         let cartProducts = Array.from(cart.querySelectorAll('.cart__product'));
-        if (!cart.querySelector('.cart__product')) {
+        let itemInCart = cartProducts.find(el => el.dataset.id === item.dataset.id);
+
+        if (itemInCart) {
+
+            // почему не удался такой синтаксис?
+            // let itemInCartQuantity = itemInCart.querySelector('.cart__product-count').textContent;
+            // ведь здесь аналогия с 
+            // let itemQuantity = Number(item.querySelector('.cart__product-count').textContent);
+
+            let itemInCartQuantity = itemInCart.querySelector('.cart__product-count');
+            itemInCartQuantity.textContent = Number(itemInCartQuantity.textContent) + itemQuantity;
+        } else {
             cart.insertAdjacentElement('beforeEnd', item);
-            console.log('add new')
-        }
-
-        for (let i = 0; i < cartProducts.length; i++) {
-            if (cartProducts[i].dataset.id === item.dataset.id) {
-                console.log('match')
-                let cartQuantityDiv = cartProducts[i].querySelector('.cart__product-count');
-                let itemQuantityDiv = item.querySelector('.cart__product-count');
-                let cartQuantity = +cartQuantityDiv.textContent;
-                let itemQuantity = +itemQuantityDiv.textContent;
-                cartQuantity += itemQuantity;
-
-                почему не получилось так? с помощью Number() тоже не подучилось
-                cartQuantity.textContent += itemQuantity.textContent;
-            } else { 
-                console.log('add another')
-                cart.insertAdjacentElement('beforeEnd', item);
-            }
         }
     }
 
@@ -60,6 +68,7 @@ for (let product of products) {
         } else if (target.classList.contains('product__quantity-control_dec')) {
             decreaseValue();
         } else if (target.classList.contains('product__add')) {
+            cartDiv.style.display = 'block';
             addProduct();
         }
     }
