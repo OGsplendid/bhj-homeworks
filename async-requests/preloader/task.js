65aed3258bde0818f9b73e9f3d59ab427ab34607
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-03-03 20:28:42 +0300

    Update task.js

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -2,11 +2,21 @@ let items = document.getElementById('items');
 let loader = document.getElementById('loader');
 
 let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
 xhr.send();
 
-xhr.onreadystatechange = function() {
+function setValute(code, value) {
+    let div = `<div class="item"><div class="item__code">${code}</div><div class="item__value">${value}</div><div class="item__currency">руб.</div></div>`;
+    items.insertAdjacentHTML('beforeend', div);
+}
+
+xhr.onreadystatechange = () => {
     if (xhr.readyState === xhr.DONE) {
-        console.log(xhr.responseText);
+        loader.classList.remove('loader_active');
+        let response = JSON.parse(xhr.response);
+        let valutes = response.response.Valute;
+        for (let valute in valutes) {
+            setValute(valutes[valute].CharCode, valutes[valute].Value);
+        }
     }
 }
