b6e07ad9028635387a8ee6dcce675257c05d0166
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-03-06 10:26:13 +0300

    Update task.js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,20 +1,34 @@
-let progress = document.getElementById('progress');
-let form = document.getElementById('form');
+const progress = document.getElementById('progress');
+const form = document.getElementById('form');
 
-form.onsubmit = function() {
-    let file = document.getElementById('file');
+form.onsubmit = function(e) {
+    e.preventDefault();
+
+    let input = this.elements.file;
+    let file = input.files[0];
+
     if (file) {
         upload(file);
     }
-    return false;
 }
 
 function upload(file) {
     let xhr = new XMLHttpRequest();
+
     xhr.upload.onprogress = function(e) {
-        console.log(e.loaded, e.total);
+        progress.value = (e.loaded / e.total).toFixed(1);
     }
+
+    xhr.onloadend = function() {
+        if (xhr.status == 200) {
+          console.log("Успех");
+        } else {
+          console.log("Ошибка " + this.status);
+        }
+    }
+    let formData = new FormData();
+    formData.append('file', file);
+
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
-    // let formData = new formData(form);
-    xhr.send(file);
+    xhr.send(formData);
 }
