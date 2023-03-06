5f6534a0b0122b0d0c34a43d5fbec1702207710d
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-03-06 16:57:12 +0300

    Update task.js

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,20 +1,41 @@
-let form = document.getElementById('signin__form');
+const form = document.getElementById('signin__form');
+let idArray = [];
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    let success;
 
     let loginForm = new FormData(form);
     let xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
     xhr.send(loginForm);
 
-    xhr.onreadystatechange = () => {
+    xhr.onload = () => {
         let response = JSON.parse(xhr.response);
-        success = response.success;
-        console.log(response)
-    }
-    if (!response.success) {
-        alert('Login or password value error');
+        let welcome = document.getElementById('welcome');
+        welcome.classList.add('welcome_active');
+        console.log(response);
+
+        if (!response.success) {
+            welcome.textContent = 'Неверный логин/пароль';
+            return;
+        }
+
+        let userId = document.getElementById('user_id');
+        userId.textContent = response.user_id;
+
+        if (idArray.find(el => el === response.user_id)) {
+            userId.textContent = idArray.find(el => el === response.user_id);
+        } else {
+            idArray.push(response.user_id);
+            localStorage.setItem('id', JSON.stringify(idArray));
+        }
+
+        form.reset();
     }
 })
+
+window.onload = () => {
+    if (localStorage.getItem('id')) {
+        idArray = JSON.parse(localStorage.getItem('id'));
+    }
+}
