4e50a9ca6ea93efc8a7cba362fe42119e55b7f45
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2023-03-07 20:21:13 +0300

    Update task.js

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,23 +2,14 @@ let button = document.getElementById('clear__all');
 let editor = document.getElementById('editor');
 
 editor.onkeyup = () => {
-    localStorage.clear();
-    localStorage.setItem('text', JSON.stringify(editor.value));
+    localStorage.setItem('text', editor.value);
 }
 
 window.onload = () => {
-    if (localStorage.getItem('text')) {
-        let storedText;
-        try {
-            storedText = JSON.parse(localStorage.getItem('text'));
-        } catch(e){
-            return null;
-        }
-        editor.value = storedText;
-    }
+    editor.value = localStorage.getItem('text');
 }
 
 button.onclick = function() {
     editor.value = '';
-    localStorage.clear();
+    localStorage.removeItem('text');
 }
